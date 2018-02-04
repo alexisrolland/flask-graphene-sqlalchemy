5dc1a41619db45067cb8fad77648c8deaa165aee
alexisrolland <alexisrolland@hotmail.com>
2018-02-04 16:05:18 +0800

    Break line

--- example/api.py
+++ example/api.py
@@ -4,7 +4,9 @@ from flask_graphql import GraphQLView
 from schema import schema
 
 app = Flask(__name__)
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
 
 @app.teardown_appcontext
