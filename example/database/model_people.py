e64f0365dbb9ab5e156ccef837f3ad7f9a145e98
alexisrolland <alexisrolland@hotmail.com>
2018-02-18 13:51:22 +0800

    Add missing data type for hair_color attribute in ModelPeople class.

--- example/database/model_people.py
+++ example/database/model_people.py
@@ -11,7 +11,7 @@ class ModelPeople(Base):
     name = Column('name', String, doc="Name of the person.")
     height = Column('height', String, doc="Height of the person.")
     mass = Column('mass', String, doc="Mass of the person.")
-    hair_color = Column('hair_color', doc="Hair color of the person.")
+    hair_color = Column('hair_color', String, doc="Hair color of the person.")
     skin_color = Column('skin_color', String, doc="Skin color of the person.")
     eye_color = Column('eye_color', String, doc="Eye color of the person.")
     birth_year = Column('birth_year', String, doc="Birth year of the person.")
